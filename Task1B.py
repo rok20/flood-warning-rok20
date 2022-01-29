6d57dc8a153d401d999d1495550098e026a1b9ab
Rudi Konrath <rok20@cam.ac.uk>
2022-01-29 19:17:39 +0000

    New changes

--- Task1B.py
+++ Task1B.py
@@ -1,10 +1,12 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_by_distance
 
-stations = build_station_list()
+def run():
+    stations = build_station_list()
 
-x = stations_by_distance(stations, (52.2053, 0.1218))
+    x = stations_by_distance(stations, (52.2053, 0.1218))
 
-print(x[:10])
-print(x[-10:])
+    print(x)
+if __name__ == "__main__":
+    run()
     
--- Task1D.py
+++ Task1D.py
@@ -2,9 +2,12 @@ from floodsystem.stationdata import build_station_list
 
 from floodsystem.geo import river_with_station
 
+def run():
+    stations = build_station_list()
+    riverswithstations = river_with_station(stations)
 
-stations = build_station_list()
-riverswithstations = river_with_station(stations)
+    print(riverswithstations)
 
-print(riverswithstations)
-
+    
+if __name__ == "__main__":
+    run()
