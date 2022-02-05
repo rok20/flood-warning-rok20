c9f9f13dd17e1863cd7fed404416f22345368f3a
Rudi Konrath <rok20@cam.ac.uk>
2022-02-05 15:53:29 +0000

    Added  a test

--- Task1F.py
+++ Task1F.py
@@ -1,10 +1,11 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.station import MonitoringStation
 from floodsystem.station import inconsistent_typical_range_stations
+from test_station import test_inconsistent_typical_range_stations
 
 def run():
     stations = build_station_list()
-    
+    test_inconsistent_typical_range_stations()
     Inconsistentlist = inconsistent_typical_range_stations(stations)
     namelist = []
     for inconsistent in Inconsistentlist:
--- test_station.py
+++ test_station.py
@@ -3,8 +3,8 @@
 # SPDX-License-Identifier: MIT
 """Unit test for the station module"""
 
-from floodsystem.station import MonitoringStation
-
+from floodsystem.station import MonitoringStation, inconsistent_typical_range_stations
+from floodsystem.stationdata import build_station_list
 
 def test_create_monitoring_station():
 
@@ -25,3 +25,8 @@ def test_create_monitoring_station():
     assert s.typical_range == trange
     assert s.river == river
     assert s.town == town
+
+def test_inconsistent_typical_range_stations():
+    stations = build_station_list()
+    x = inconsistent_typical_range_stations(stations)
+    assert len(x)>0
