e28205f9b57f28afc37396183ba1702708873250
Rudi Konrath <rok20@cam.ac.uk>
2022-02-12 21:24:49 +0000

    changes

--- Task2C.py
+++ Task2C.py
@@ -1,6 +1,13 @@
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.stationdata import build_station_list
 
-stations = build_station_list()
-x = stations_highest_rel_level(stations, 10)
-print(x)
+def run():
+    # Build list of stations
+    stations = build_station_list()
+    
+    x = stations_highest_rel_level(stations, 10)
+    for item in x:
+        print(item[0], item[1])
+
+if __name__ == "__main__":
+    run()
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,40 +1,33 @@
-from floodsystem.stationdata import update_water_levels
-from floodsystem.utils import sorted_by_key
 from . import datafetcher
 from . import stationdata
 from .station import MonitoringStation 
 from .station import inconsistent_typical_range_stations
-
+from .utils import sorted_by_key
 
 def stations_highest_rel_level(stations, N):
-<<<<<<< HEAD
     stationdata.update_water_levels(stations)
+    #Create a list, and ensure using no None data, then make a list including the name and the difference between current level and the relative water level
 
     rel_level = []
     for station in stations:
-        if station not in inconsistent_typical_range_stations(stations):
-            if station.latest_level != None:
-                x = station.typical_range
+        if station.latest_level != None:
+            if station.relative_water_level() != None:
+                x = station.relative_water_level()
                 y = station.latest_level
-            
-                rel_level.append((station.name, y - x[1]))
+                
+                rel_level.append((station.name, y-x))
+    #Sort the list from highest according to the most at risk. Then extract the first N
+
     rel_level.sort(key=lambda x:x[1], reverse=True)
     rel_level_final = rel_level[:N]
 
     return rel_level_final
     
     
-        
-
-
-
-=======
-    data = datafetcher.fetch_latest_water_level_data()
-
 #Take in a list of MonitoringStation objects and a tol value
 #return a list of tuples (station with rel water level > tol, rel), sorted by rel high to low
 def stations_level_over_threshold(stations, tol):
-    update_water_levels(stations)
+    stationdata.update_water_levels(stations)
     #create empty list
     x = []
     for station in stations:
@@ -44,4 +37,6 @@ def stations_level_over_threshold(stations, tol):
             if rel > tol:
                 x.append((station, rel))
     return sorted_by_key(x, 1, True)
->>>>>>> 436456f5880a8a6ae6a02bfb077305b7c0037db3
+
+
+
