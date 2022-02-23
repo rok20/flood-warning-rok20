db0cd25fe405895b3a6f2fde411bbfb0b1447fc6
Rudi Konrath <rok20@cam.ac.uk>
2022-02-23 22:02:17 +0000

    working on G

--- Task2G.py
+++ Task2G.py
@@ -3,22 +3,59 @@
 #High rising and above the top band/ 
 #Moderate rising and between the two bands/ decreasing but above the highest
 #Low not rising and not above usual heighest
+#wants speeding up
+
 
 #write a function to determine gradient of line? Since the polyfit is set so 0 is present, the x coefficient is the gradient at present day
+import string
 from floodsystem.analysis import polyfit
 from floodsystem.stationdata import build_station_list
 from floodsystem.datafetcher import fetch_measure_levels
 import datetime
 
+
 stations = build_station_list()
 listforgrad = []
 dt = 1
-for station in stations:
-    if station.measure_id != None:
-        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
-        listforgrad.append((station.name, dates, levels))
-    
 def gradient(dates, levels):
+    
     list = polyfit(dates, levels, 4)
-    print(list)
+    return list
+    
+gradientlist = []
+for station in stations:
+    #print(type(station.measure_id))
+    if type(station.measure_id) == str:
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        try:
+            polynomial = gradient(dates, levels)
+        
+            gradientlist.append([station.name, polynomial[0][1]])
+            
+        except:
+            pass
+#consider rising/falling significantly, say 0.5 for now?
+greaterthanhalf = []
+lessthanhalf = []
+betweenthetwo = []
+for item in gradientlist:
+    if item[1] > 0.5:
+        greaterthanhalf.append(item)
+    elif item[1] < 0.5:
+        lessthanhalf.append(item)
+    else:
+        betweenthetwo.append(item)
+
+#Printing to show it works, this data is suitably sorted now for the purpose :) 
+# now need to look at speeding it up, and the current levels to then sort into four categories.
+#  Definitely needs speeding up!!!
+
+print(greaterthanhalf)
+print(lessthanhalf)
+print(betweenthetwo)
+    
+
+
+
+   
 
--- floodsystem/datafetcher.py
+++ floodsystem/datafetcher.py
@@ -129,12 +129,16 @@ def fetch_measure_levels(measure_id, dt):
 
     # Extract dates and levels
     dates, levels = [], []
-    for measure in data['items']:
-        # Convert date-time string to a datetime object
-        d = dateutil.parser.parse(measure['dateTime'])
-
-        # Append data
-        dates.append(d)
-        levels.append(measure['value'])
-
+    try:
+        for measure in data['items']:
+        
+            # Convert date-time string to a datetime object
+            d = dateutil.parser.parse(measure['dateTime'])
+            
+            # Append data
+            dates.append(d)
+            levels.append(measure['value'])
+            
+    except:
+        pass
     return dates, levels
