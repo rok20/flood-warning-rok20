cf8515ccb64ef118cfd2e8e543d6b115d05fd1c8
Rudi Konrath <rok20@cam.ac.uk>
2022-02-24 12:59:41 +0000

    Needs work

--- Task2G.py
+++ Task2G.py
@@ -12,6 +12,7 @@ from floodsystem.analysis import polyfit
 from floodsystem.stationdata import build_station_list
 from floodsystem.datafetcher import fetch_measure_levels
 import datetime
+import floodsystem.datafetcher
 
 
 stations = build_station_list()
@@ -27,32 +28,52 @@ for station in stations:
     #print(type(station.measure_id))
     if type(station.measure_id) == str:
         dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        
         try:
             polynomial = gradient(dates, levels)
-        
-            gradientlist.append([station.name, polynomial[0][1]])
+            
+            #make a list of tuples with station name, the gradient and the current level and typical range: LATEST LEVEL isnt working 
+            gradientlist.append([station.name, polynomial[0][1], levels[0], station.typical_range])
             
         except:
             pass
-#consider rising/falling significantly, say 0.5 for now?
-greaterthanhalf = []
-lessthanhalf = []
-betweenthetwo = []
-for item in gradientlist:
-    if item[1] > 0.5:
-        greaterthanhalf.append(item)
-    elif item[1] < 0.5:
-        lessthanhalf.append(item)
-    else:
-        betweenthetwo.append(item)
 
-#Printing to show it works, this data is suitably sorted now for the purpose :) 
-# now need to look at speeding it up, and the current levels to then sort into four categories.
+# Four categories and then look at initial conditions to sort into 4 groups ?
+severe = []
+high = []
+moderate = []
+low = []
+print(gradientlist)
+for item in gradientlist:
+    try:
+        if item[1] > 0 and item[2] > 5* item[3][1]:
+            severe.append(item[0])
+        elif item[1] > 0 and item[2] < 5* item[3][1] and item[2] > item[3][1] or item[1] <0 and item[2] > 5* item[3][1]:
+            high.append(item[0])
+        elif item[1] < 0 and item[2] < 3* item[3][1] and item[2] > item[3][1] or item[1] > 0 and item[2] > item[3][0] and item[2] < item[3][1]:
+            moderate.append(item[0])
+        elif item[1] < 0 and item[2] > item[3][0] and item[2] < item[3][1] or item[1] > 0 and item[2] < item[3][0]:
+            low.append(item[0])
+    except:
+        pass
+
 #  Definitely needs speeding up!!!
 
-print(greaterthanhalf)
-print(lessthanhalf)
-print(betweenthetwo)
+print("Stations with severe risk:")
+print(severe)
+
+print("Stations with high risk:")
+print(high)
+
+print("Stations with moderate risk:")
+print(moderate)
+
+print("Stations with low risk:")
+print(low)
+
+
+
+
     
 
 
