7b5e42d0f6750d2509253d3840e7d29b0e01cef3
Yilin299 <98100049+Yilin299@users.noreply.github.com>
2022-02-26 18:01:34 +0000

    modified task 2g a lot

--- Task2G.py
+++ Task2G.py
@@ -10,37 +10,30 @@
 import string
 from time import time
 from floodsystem.analysis import polyfit
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.datafetcher import fetch_measure_levels
 import datetime
 import floodsystem.datafetcher
 from numba import jit
+from floodsystem.flood import stations_level_over_threshold
+import floodsystem.station
 
+#My definition for the different levels of flooding:
+#condition for already flooding: relative water level (hereby rel) > 1
+#low risk: rel < tol_low,set at 0.8
+#moderate risk: tol_low < rel< 1, and currently not increasing (potentially can flood, not yet)
+#high risk: 1. tol_low < rel < 1, and is increasing; 2. rel > 1, and not increasing
+#severe: rel > 1, and increasing
 
 stations = build_station_list()
 listforgrad = []
-dt = 1
-#def gradient(dates, levels):
-    
-    #list = polyfit(dates, levels, 4)
-    #return list
+#fetch data for only the past 3 hrs
+dt = 0.125
 
-gradientlist = []
-#gradientlist = [(station.name, gradient(fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt)))[0][1], fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))[1][0], station.typical_range) for station in stations]
-for station in stations:
-    
-    
-    
-    dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
-        
-    try:
-        polynomial = polyfit(dates, levels, 4)
-            
-        #make a list of tuples with station name, the gradient and the current level and typical range: LATEST LEVEL isnt working 
-        gradientlist.append([station.name, polynomial[0][1], levels[0], station.typical_range])
-            
-    except:
-        pass
+#tolerance relative water level for low risk
+tol_low = 0.8
+#gradient threshold
+k_0 = 0
 
 # Four categories and then look at initial conditions to sort into 4 groups ?
 severe = []
@@ -48,32 +41,83 @@ high = []
 moderate = []
 low = []
 
-for item in gradientlist:
+#to make code clear
+def print_station_names(stations):
+    for station in stations:
+        print(station.name, end=' ')
+    print()
+
+#def gradient(dates, levels):
+    
+    #list = polyfit(dates, levels, 4)
+    #return list
+
+
+
+#preprocess station data to decrease number of stations for further inspection
+#exclude and separate low risk stations, they do not need more calculations
+exclude_low = []
+for item in stations_level_over_threshold(stations, tol_low):
+    exclude_low.append(item[0])
+for station in stations:
+    if station not in exclude_low:
+        low.append(station)
+
+update_water_levels(exclude_low)
+
+gradientlist = []
+#Test how long it takes to calculate gradients
+#print('Starting gradient calculation')
+#gradientlist = [(station.name, gradient(fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt)))[0][1], fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))[1][0], station.typical_range) for station in stations]
+for station in exclude_low:    
+    dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+
     try:
-        if item[1] > 0 and item[2] > 5* item[3][1]:
-            severe.append(item[0])
-        elif item[1] > 0 and item[2] < 5* item[3][1] and item[2] > item[3][1] or item[1] <0 and item[2] > 5* item[3][1]:
-            high.append(item[0])
-        elif item[1] < 0 and item[2] < 3* item[3][1] and item[2] > item[3][1] or item[1] > 0 and item[2] > item[3][0] and item[2] < item[3][1]:
-            moderate.append(item[0])
-        elif item[1] < 0 and item[2] > item[3][0] and item[2] < item[3][1] or item[1] > 0 and item[2] < item[3][0]:
-            low.append(item[0])
+        polynomial = polyfit(dates, levels, 1)
+        #make a list of tuples with station itself and the linear gradient of the last 3 hrs 
+        gradientlist.append((station, polynomial[0][0]))
     except:
         pass
 
+#print('Finished gradient calculation')
+
+
+
+
+#print('start classifying')
+for item in gradientlist:
+    rel = item[0].relative_water_level()
+    k = item[1]
+    try:
+        if rel < 1:
+            if k < k_0:
+                moderate.append(item[0])
+            elif k >= k_0:
+                high.append(item[0])
+        elif rel >= 1:
+            if k < k_0:
+                high.append(item[0])
+            elif k >= k_0:
+                severe.append(item[0])
+    except:
+        pass
+#print('finished classifying')
 #  Definitely needs speeding up!!!
+#speeding up~
 
 print("Stations with severe risk:")
-print(severe)
+print_station_names(severe)
 
 print("Stations with high risk:")
-print(high)
+print_station_names(high)
 
 print("Stations with moderate risk:")
-print(moderate)
+print_station_names(moderate)
 
 print("Stations with low risk:")
-print(low)
+print_station_names(low)
+
+
 
 
 
